37e1a5d2297a082ac7a03306fe8588036b9995f8
Roonglit <roonglit@gmail.com>
2024-09-21 08:41:54 +0700

    add practice for Class section

--- 05_class/access_control2.rb
+++ 05_class/access_control2.rb
@@ -1,13 +1,18 @@
 class Person
   attr_accessor :name, :age
 
+  attr_accessor :secret
+
+  private :secret
+
   # Initialize with name and age
   def initialize(name, age)
     @name = name
     @age = age
+    @secret = "I scared of spiders."
   end
 
-  # Public method to compare age with another person
+  # Public method to compare age with another person  
   def older_than?(other_person)
     if compare_age(other_person)
       puts "#{name} is older than #{other_person.name}."
@@ -16,11 +21,22 @@ class Person
     end
   end
 
+  def discuss_secret_with(other_person)
+    puts other_person.share_secret
+
+    # This will raise a NoMethodError
+    # puts other_person.secret
+  end
+
   protected
 
   # Protected method to compare age, accessible within the class and its instances
   def compare_age(other_person)
     age > other_person.age
+  end
+
+  def share_secret
+    secret
   end
 end
 
@@ -28,12 +44,18 @@ end
 person1 = Person.new("Alice", 30)
 person2 = Person.new("Bob", 25)
 
-# Compare their ages using a public method
-person1.older_than?(person2)  # Output: Alice is older than Bob.
-person2.older_than?(person1)  # Output: Bob is not older than Alice.
+# Trying to call a private method directly will result in an error
+# puts person1.secret
 
 # Trying to call a protected method directly will result in an error
 # person1.compare_age(person2)  # This will raise a NoMethodError
+# person1.share_secret
+
+person1.discuss_secret_with(person2)
+
+# Compare their ages using a public method
+person1.older_than?(person2)  # Output: Alice is older than Bob.
+person2.older_than?(person1)  # Output: Bob is not older than Alice.
 
 
 class Employee < Person
@@ -46,7 +68,7 @@ class Employee < Person
   end
 end
 
-# Create two Employee objects
+# # Create two Employee objects
 employee1 = Employee.new("Charlie", 40)
 employee2 = Employee.new("Dave", 35)
 
---
+++ 05_class/practice.rb
@@ -0,0 +1,25 @@
+# create a new Class, User, that has the following attributes:
+# - name
+# - email
+# - password
+
+
+# create a new Class, Room, that has the following attributes:
+# - name
+# - description
+# - users
+
+# create a new Class, Message, that has the following attributes:
+# - user
+# - room
+# - content
+
+# add a method to user so, user can enter to a room
+# user.enter_room(room)
+
+# add a method to user so, user can send a message to a room
+# user.send_message(room, message)
+# user.ackowledge_message(room, message)
+
+# add a method to a room, so it can broadcast a message to all users
+# room.broadcast(message)
